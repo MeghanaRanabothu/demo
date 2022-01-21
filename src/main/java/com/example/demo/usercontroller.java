c42e6334e4bf95a83e4164bf624a9c511dd39c75
MeghanaRanabothu <meghanaranabothu@gmail.com>
2022-01-21 10:33:33 +0530

    final comit demo project

---
+++ src/main/java/com/example/demo/usercontroller.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.user;
+
+@RestController
+public class usercontroller {
+   @GetMapping("/user")
+	String getUser() {
+		System.out.println("testing");
+		return "testing";
+   }
+   @PostMapping("/user")
+		 void createUser (@RequestBody user User) {
+			 System.out.println(User.getName());
+	}
+
+
+}
